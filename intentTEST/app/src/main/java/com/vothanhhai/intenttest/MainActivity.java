07fb3542f977d03bceb0584953b9b4cd60709892
SennKuu02 <SennKuu02>
2022-09-05 23:46:10 +0700

    05/9

--- intentTEST/app/src/main/java/com/vothanhhai/intenttest/MainActivity.java
+++ intentTEST/app/src/main/java/com/vothanhhai/intenttest/MainActivity.java
@@ -1,14 +1,26 @@
 package com.vothanhhai.intenttest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
+public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnOpen=(Button)
+                findViewById(R.id.button);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View arg0) {
+               doOpenChildActivity();
+           }
+       });
     }
-}
+    public void doOpenChildActivity()
+    {
+        Intent myIntent=new Intent(MainActivity.this, ChildActivity.class);
+        startActivity(myIntent);
+    }}
