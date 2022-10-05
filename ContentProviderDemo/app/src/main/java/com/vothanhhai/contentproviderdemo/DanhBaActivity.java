1b9c897d2f0e4b08b8d3f647f5951c2ef2233730
SennKuu02 <SennKuu02>
2022-10-05 23:24:47 +0700

    10/5

---
+++ ContentProviderDemo/app/src/main/java/com/vothanhhai/contentproviderdemo/DanhBaActivity.java
@@ -0,0 +1,66 @@
+package com.vothanhhai.contentproviderdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.vothanhhai.model.Contact;
+
+import java.util.ArrayList;
+
+import com.vothanhhai.model.Contact;
+
+public class DanhBaActivity extends AppCompatActivity {
+
+    ListView lvDanhBa;
+    ArrayList<Contact>dsDanhBa;
+    ArrayAdapter<Contact>adapterDanhBa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_ba);
+
+        addControls();
+        addEvents();
+
+        showAllContactFromDevice();
+    }
+
+    private void showAllContactFromDevice() {
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+        dsDanhBa.clear();
+        while (cursor.moveToNext()){
+            String tenCotName=ContactsContract.Contacts.DISPLAY_NAME;
+            String tenCotPhone=ContactsContract.CommonDataKinds.Phone.NUMBER;
+            int vtTenCotName=cursor.getColumnIndex(tenCotName);
+            int vtTenCotPhone=cursor.getColumnIndex(tenCotPhone);
+            String name=cursor.getString(vtTenCotName);
+            String phone=cursor.getString(vtTenCotPhone);
+            Contact contact = new Contact(name,phone);
+            dsDanhBa.add(contact);
+        }
+        adapterDanhBa.notifyDataSetChanged();
+    }
+
+    private void addEvents() {
+    }
+
+    private void addControls() {
+        lvDanhBa = findViewById(R.id.lv_danhba);
+        dsDanhBa = new ArrayList<>();
+        adapterDanhBa = new ArrayAdapter<Contact>(
+                DanhBaActivity.this,
+                android.R.layout.simple_list_item_1,dsDanhBa);
+        lvDanhBa.setAdapter(adapterDanhBa);
+
+    }
+
+}
