258af45908b27f3f6c38133a72771cf5836230a1
SennKuu02 <SennKuu02>
2022-10-24 19:39:24 +0700

    24/10

---
+++ ContentProviderDemo/app/src/main/java/com/vothanhhai/Detail.java
@@ -0,0 +1,16 @@
+package com.vothanhhai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vothanhhai.contentproviderdemo.R;
+
+public class Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- ContentProviderDemo/app/src/main/java/com/vothanhhai/contentproviderdemo/DanhBaActivity.java
+++ ContentProviderDemo/app/src/main/java/com/vothanhhai/contentproviderdemo/DanhBaActivity.java
@@ -3,13 +3,17 @@ package com.vothanhhai.contentproviderdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.vothanhhai.Detail;
 import com.vothanhhai.model.Contact;
 
 import java.util.ArrayList;
@@ -26,12 +30,22 @@ public class DanhBaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danh_ba);
-
         addControls();
         addEvents();
 
+        lvDanhBa.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i==0) {
+                    startActivity(new Intent(DanhBaActivity.this,Detail.class));
+                }
+
+            }
+        });
+
         showAllContactFromDevice();
     }
+
 
     private void showAllContactFromDevice() {
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
